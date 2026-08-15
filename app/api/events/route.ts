cd522e988b4d902f1c6d59c8b5ec1820cbf5d29a
Samroj Khan <samrojk7744@gmail.com>
2026-08-15 21:56:23 +0530

    api endpoints are working fine now

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,12 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectToDatabase from "@/lib/mongodb";
-import { Event } from "@/database";
+import { Event } from "@/database/event.model";
 
 export async function POST(req: NextRequest) {
   try {
     await connectToDatabase();
 
-    const eventData = await req.json();
+    // const eventData = await req.json();
+    const formData = await req.formData();
+    const eventData = Object.fromEntries(formData.entries());
 
     const createdEvent = await Event.create(eventData);
 
